522527bd1a41bc76268560b61872fae943e71db1
lincot <lincot@disroot.org>
2022-03-28 08:36:25 +0300

    init

---
+++ src/main.ts
@@ -0,0 +1,378 @@
+import {
+  Connection,
+  Keypair,
+  PublicKey,
+  sendAndConfirmTransaction,
+  SystemProgram,
+  Transaction,
+  TransactionInstruction,
+} from "@solana/web3.js";
+import {
+  approve,
+  createAccount,
+  createMint,
+  mintTo,
+  TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
+import BN from "bn.js";
+const BufferLayout = require("@solana/buffer-layout");
+
+const TOKEN_SWAP_PROGRAM_ID: PublicKey = new PublicKey(
+  "SwaPpA9LAaLfeLi3a68M4DjnLqgtticKg6CnyNwgAC8",
+);
+
+const connection = new Connection("https://api.devnet.solana.com", "recent");
+const owner = new Keypair();
+const feeOwner = new Keypair();
+const payer = new Keypair();
+const swapAccount = new Keypair();
+let swapAuthority: PublicKey;
+let mintA: PublicKey;
+let mintB: PublicKey;
+let mintPool: PublicKey;
+let accountA: PublicKey;
+let accountB: PublicKey;
+let accountDestination: PublicKey;
+let accountFee: PublicKey;
+
+function createInitSwapInstruction(
+  swapAccount: Keypair,
+  swapAuthority: PublicKey,
+  accountA: PublicKey,
+  accountB: PublicKey,
+  mintPool: PublicKey,
+  accountFee: PublicKey,
+  accountDestination: PublicKey,
+  tradeFeeNumerator: number,
+  tradeFeeDenominator: number,
+  ownerTradeFeeNumerator: number,
+  ownerTradeFeeDenominator: number,
+  ownerWithdrawFeeNumerator: number,
+  ownerWithdrawFeeDenominator: number,
+  hostFeeNumerator: number,
+  hostFeeDenominator: number,
+  curveType: number,
+  curveParameters: BN,
+): TransactionInstruction {
+  const keys = [
+    { pubkey: swapAccount.publicKey, isSigner: true, isWritable: true },
+    { pubkey: swapAuthority, isSigner: false, isWritable: false },
+    { pubkey: accountA, isSigner: false, isWritable: false },
+    { pubkey: accountB, isSigner: false, isWritable: false },
+    { pubkey: mintPool, isSigner: false, isWritable: true },
+    { pubkey: accountFee, isSigner: false, isWritable: false },
+    { pubkey: accountDestination, isSigner: false, isWritable: true },
+    { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
+  ];
+  const commandDataLayout = BufferLayout.struct([
+    BufferLayout.u8("instruction"),
+    BufferLayout.nu64("tradeFeeNumerator"),
+    BufferLayout.nu64("tradeFeeDenominator"),
+    BufferLayout.nu64("ownerTradeFeeNumerator"),
+    BufferLayout.nu64("ownerTradeFeeDenominator"),
+    BufferLayout.nu64("ownerWithdrawFeeNumerator"),
+    BufferLayout.nu64("ownerWithdrawFeeDenominator"),
+    BufferLayout.nu64("hostFeeNumerator"),
+    BufferLayout.nu64("hostFeeDenominator"),
+    BufferLayout.u8("curveType"),
+    BufferLayout.blob(32, "curveParameters"),
+  ]);
+  let data = Buffer.alloc(1024);
+
+  const curveParamsBuffer = Buffer.alloc(32);
+  curveParameters.toBuffer().copy(curveParamsBuffer);
+
+  const encodeLength = commandDataLayout.encode(
+    {
+      instruction: 0,
+      tradeFeeNumerator,
+      tradeFeeDenominator,
+      ownerTradeFeeNumerator,
+      ownerTradeFeeDenominator,
+      ownerWithdrawFeeNumerator,
+      ownerWithdrawFeeDenominator,
+      hostFeeNumerator,
+      hostFeeDenominator,
+      curveType,
+      curveParameters: curveParamsBuffer,
+    },
+    data,
+  );
+  data = data.slice(0, encodeLength);
+
+  return new TransactionInstruction({
+    keys,
+    programId: TOKEN_SWAP_PROGRAM_ID,
+    data,
+  });
+}
+
+async function createTokenSwap(
+  connection: Connection,
+  payer: Keypair,
+  swapAccount: Keypair,
+  swapAuthority: PublicKey,
+  accountA: PublicKey,
+  accountB: PublicKey,
+  mintPool: PublicKey,
+  accountFee: PublicKey,
+  accountDestination: PublicKey,
+  tradeFeeNumerator: number,
+  tradeFeeDenominator: number,
+  ownerTradeFeeNumerator: number,
+  ownerTradeFeeDenominator: number,
+  ownerWithdrawFeeNumerator: number,
+  ownerWithdrawFeeDenominator: number,
+  hostFeeNumerator: number,
+  hostFeeDenominator: number,
+  curveType: number,
+  curveParameters: BN,
+) {
+  const POOL_SPACE = 3 * 1 + 32 * 7 + 8 * 8 + 1 + 32;
+
+  const transaction = new Transaction();
+  transaction.add(
+    SystemProgram.createAccount({
+      fromPubkey: payer.publicKey,
+      newAccountPubkey: swapAccount.publicKey,
+      lamports: await connection.getMinimumBalanceForRentExemption(
+        POOL_SPACE,
+      ),
+      space: POOL_SPACE,
+      programId: TOKEN_SWAP_PROGRAM_ID,
+    }),
+  );
+
+  const instruction = createInitSwapInstruction(
+    swapAccount,
+    swapAuthority,
+    accountA,
+    accountB,
+    mintPool,
+    accountFee,
+    accountDestination,
+    tradeFeeNumerator,
+    tradeFeeDenominator,
+    ownerTradeFeeNumerator,
+    ownerTradeFeeDenominator,
+    ownerWithdrawFeeNumerator,
+    ownerWithdrawFeeDenominator,
+    hostFeeNumerator,
+    hostFeeDenominator,
+    curveType,
+    curveParameters,
+  );
+  transaction.add(instruction);
+
+  await sendAndConfirmTransaction(
+    connection,
+    transaction,
+    [payer, swapAccount],
+  );
+}
+
+async function createMints() {
+  mintA = await createMint(
+    connection,
+    payer,
+    owner.publicKey,
+    null,
+    2,
+  );
+  mintB = await createMint(
+    connection,
+    payer,
+    owner.publicKey,
+    null,
+    2,
+  );
+}
+
+async function createPool() {
+  [swapAuthority] = await PublicKey.findProgramAddress(
+    [swapAccount.publicKey.toBuffer()],
+    TOKEN_SWAP_PROGRAM_ID,
+  );
+
+  mintPool = await createMint(
+    connection,
+    payer,
+    swapAuthority,
+    null,
+    2,
+  );
+  accountDestination = await createAccount(
+    connection,
+    payer,
+    mintPool,
+    owner.publicKey,
+  );
+  accountFee = await createAccount(
+    connection,
+    payer,
+    mintPool,
+    feeOwner.publicKey,
+  );
+
+  accountA = await createAccount(
+    connection,
+    payer,
+    mintA,
+    swapAuthority,
+    new Keypair(),
+  );
+  await mintTo(connection, payer, mintA, accountA, owner, 1_000);
+
+  accountB = await createAccount(
+    connection,
+    payer,
+    mintB,
+    swapAuthority,
+    new Keypair(),
+  );
+  await mintTo(connection, payer, mintB, accountB, owner, 1_000);
+
+  await createTokenSwap(
+    connection,
+    payer,
+    swapAccount,
+    swapAuthority,
+    accountA,
+    accountB,
+    mintPool,
+    accountFee,
+    accountDestination,
+    25,
+    10000,
+    5,
+    10000,
+    0,
+    0,
+    20,
+    100,
+    0, // constant product
+    new BN(1_000_000),
+  );
+}
+
+function createSwapInstruction(
+  swapAccount: PublicKey,
+  authority: PublicKey,
+  userTransferAuthority: PublicKey,
+  userSource: PublicKey,
+  poolSource: PublicKey,
+  poolDestination: PublicKey,
+  userDestination: PublicKey,
+  poolMint: PublicKey,
+  feeAccount: PublicKey,
+  hostFeeAccount: PublicKey | null,
+  amountIn: number,
+  minimumAmountOut: number,
+): TransactionInstruction {
+  const dataLayout = BufferLayout.struct([
+    BufferLayout.u8("instruction"),
+
+    BufferLayout.blob(8, "amountIn"),
+    BufferLayout.blob(8, "minimumAmountOut"),
+  ]);
+
+  const data = Buffer.alloc(dataLayout.span);
+  dataLayout.encode(
+    {
+      instruction: 1, // Swap instruction
+      amountIn: new BN(amountIn).toBuffer(),
+      minimumAmountOut: new BN(minimumAmountOut).toBuffer(),
+    },
+    data,
+  );
+
+  const keys = [
+    { pubkey: swapAccount, isSigner: false, isWritable: false },
+    { pubkey: authority, isSigner: false, isWritable: false },
+    { pubkey: userTransferAuthority, isSigner: true, isWritable: false },
+    { pubkey: userSource, isSigner: false, isWritable: true },
+    { pubkey: poolSource, isSigner: false, isWritable: true },
+    { pubkey: poolDestination, isSigner: false, isWritable: true },
+    { pubkey: userDestination, isSigner: false, isWritable: true },
+    { pubkey: poolMint, isSigner: false, isWritable: true },
+    { pubkey: feeAccount, isSigner: false, isWritable: true },
+    { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
+  ];
+  if (hostFeeAccount !== null) {
+    keys.push({ pubkey: hostFeeAccount, isSigner: false, isWritable: true });
+  }
+  return new TransactionInstruction({
+    keys,
+    programId: TOKEN_SWAP_PROGRAM_ID,
+    data,
+  });
+}
+
+async function swap() {
+  const user = new Keypair();
+  const userAccountA = await createAccount(
+    connection,
+    payer,
+    mintA,
+    user.publicKey,
+    new Keypair(),
+  );
+  await mintTo(connection, payer, mintA, userAccountA, owner, 50);
+  const userTransferAuthority = new Keypair();
+  await approve(
+    connection,
+    payer,
+    userAccountA,
+    userTransferAuthority.publicKey,
+    user,
+    50,
+  );
+  const userAccountB = await createAccount(
+    connection,
+    payer,
+    mintB,
+    user.publicKey,
+    new Keypair(),
+  );
+  const hostFeeAccount = await createAccount(
+    connection,
+    payer,
+    mintPool,
+    user.publicKey,
+  );
+
+  await sendAndConfirmTransaction(
+    connection,
+    new Transaction().add(
+      createSwapInstruction(
+        swapAccount.publicKey,
+        swapAuthority,
+        userTransferAuthority.publicKey,
+        userAccountA,
+        accountA,
+        accountB,
+        userAccountB,
+        mintPool,
+        accountFee,
+        hostFeeAccount,
+        50,
+        45,
+      ),
+    ),
+    [payer, userTransferAuthority],
+  );
+}
+
+async function main() {
+  await connection.confirmTransaction(
+    await connection.requestAirdrop(
+      payer.publicKey,
+      1_000_000_000,
+    ),
+  );
+  await createMints();
+  await createPool();
+  await swap();
+}
+
+main();
